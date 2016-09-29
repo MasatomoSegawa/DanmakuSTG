0f60bf7a374c55c3d97fe34830b3d38631aa8569
Shibainu <masa-segawa@masa-segawa.com>
2016-09-30 01:56:15 +0900

    とりあえず追尾するメソッドを作成した

---
+++ Assets/Bullet.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public Rigidbody2D myRigidbody2D;
+
+}
---
+++ Assets/Common/Script/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public Bullet bullet;
+
+	public float bulletSpeed;
+
+	public float shootDuration;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.A)) {
+			StartCoroutine (Shoot());
+		}
+
+	}
+
+	IEnumerator Shoot() {
+
+		for (var i = 0; i < 10; i++) {
+
+			Bullet tmpBullet = Instantiate (bullet) as Bullet;
+			tmpBullet.transform.position = this.gameObject.transform.position;
+
+			Vector3 moveVector = Player.Instance.transform.position - this.gameObject.transform.position;
+			moveVector = moveVector.normalized;
+
+			tmpBullet.myRigidbody2D.velocity = moveVector * bulletSpeed;
+
+			yield return new WaitForSeconds (shootDuration);
+		}
+			
+	}
+
+}
